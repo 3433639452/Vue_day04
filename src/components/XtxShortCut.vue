da26a06cea756724c27ae124e590555612742e7f
Songyixuan2002 <3433639452@qq.com>
2024-12-25 11:27:42 +0800

    src

---
+++ src/components/XtxShortCut.vue
@@ -0,0 +1,35 @@
+<template>
+<!-- 结构 -->
+  <div class="App">
+    <!-- 快捷链接  -->
+    <div class="shortcut">
+      <div class="wrapper">
+        <ul>
+          <li><a href="#" class="login">请先登录</a></li>
+          <li><a href="#">免费注册</a></li>
+          <li><a href="#">我的订单</a></li>
+          <li><a href="#">会员中心</a></li>
+          <li><a href="#">帮助中心</a></li>
+          <li><a href="#">在线客服</a></li>
+          <li>
+            <a href="#"
+              ><span class="iconfont icon-mobile-phone"></span>手机版</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+    </div>
+</template>
+
+<script>
+// 逻辑
+export default {
+
+}
+</script>
+
+<style scoped>
+/* 样式 */
+
+</style>
